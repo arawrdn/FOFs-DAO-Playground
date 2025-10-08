ffde310f9716deda1d6813f75d8fde8c9bed833f
0xward <aradeawardana97@gmail.com>
2025-10-08 13:23:06 +0700

    Create fofs-theme.js

---
+++ src/fofs-theme.js
@@ -0,0 +1,8 @@
+export function applyFofsTheme() {
+  console.log(`
+  🌟 Fairly Odd Fellas DAO Playground 🌟
+  ======================================
+  Welcome to the wacky Web3 universe!
+  Cast your votes and shape the DAO future.
+  `);
+}
