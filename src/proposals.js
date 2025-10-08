53fefe60fe75408a8c33205fc1abed34dc01de64
0xward <aradeawardana97@gmail.com>
2025-10-08 13:22:34 +0700

    Create proposals.js

---
+++ src/proposals.js
@@ -0,0 +1,14 @@
+export const proposals = [
+  {
+    title: "Add New Fairly Odd Fellas NFT Collection",
+    description: "Proposal to integrate NFT collection CA: 0x049ee6d2249c242611e1b704f0babaa8157d69eb"
+  },
+  {
+    title: "Treasury Allocation",
+    description: "Distribute 10% of treasury funds to community development."
+  },
+  {
+    title: "Partnership with On-chain Game",
+    description: "Form a partnership with an on-chain game project."
+  }
+];
